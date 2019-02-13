88c3d3727df3289e22f3378ddd9dea7b1ee60f03
linjiayu <linjiayu@meituan.com>
2019-02-13 17:38:14 +0800

    Updates

---
+++ precache-manifest.3ffef3bef5a6528f0e81d584f3da37f1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7021f73db7767018f09d",
+    "url": "/static/js/main.7021f73d.chunk.js"
+  },
+  {
+    "revision": "681d41296bf467a8527a",
+    "url": "/static/js/1.681d4129.chunk.js"
+  },
+  {
+    "revision": "09ac8d17cea7c2c106f227a09ffef461",
+    "url": "/index.html"
+  }
+];
